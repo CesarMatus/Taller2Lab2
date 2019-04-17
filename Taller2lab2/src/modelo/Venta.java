a3fc603a8f208448d9d1d7d2aee2254b73d008eb
SofiaRivera <s.rivera05@ufromail.cl>
2019-04-17 16:20:08 -0400

    actualizacionVenta
    act.venta

--- Taller2lab2/src/modelo/Venta.java
+++ Taller2lab2/src/modelo/Venta.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Date;
+
+
 public class Venta {
+    private String vendedor;
+    private String sucursal;
+    private Date fecha;
+    private int monto;
+
+    public Venta(String vendedor, String sucursal, Date fecha, int monto) {
+        this.vendedor = vendedor;
+        this.sucursal = sucursal;
+        this.fecha = fecha;
+        this.monto = monto;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(String sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+    
     
 }
