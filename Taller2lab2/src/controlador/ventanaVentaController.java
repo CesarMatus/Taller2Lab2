0c03123575227a91e0e377be6bfe03482976c9e6
SofiaRivera <s.rivera05@ufromail.cl>
2019-04-17 16:44:42 -0400

    ventana controller

---
+++ Taller2lab2/src/controlador/ventanaVentaController.java
@@ -0,0 +1,24 @@
+
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.ventanaVenta;
+
+
+public class ventanaVentaController implements ActionListener{
+    
+    private ventanaVenta vv;
+    
+    public ventanaVentaController(ventanaVenta vv) {
+        this.vv=vv;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+    
+    
+}
