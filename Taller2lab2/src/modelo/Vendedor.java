28f404d66f2a43f8c112176510d69f7910430556
CesarMatus <46377900+CesarMatus@users.noreply.github.com>
2019-04-17 16:06:50 -0400

    vendedor

--- Taller2lab2/src/modelo/Vendedor.java
+++ Taller2lab2/src/modelo/Vendedor.java
@@ -10,5 +10,49 @@ package modelo;
  * @author cesar
  */
 public class Vendedor {
+    private int id; 
+    private String nombre;
+    private String apellido; 
+    private String rut; 
+
+    public Vendedor(int id, String nombre, String apellido, String rut) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+    
     
 }
