33419f6ddc455eb7ae0921ba06a7c8bee4b1790c
SofiaRivera <s.rivera05@ufromail.cl>
2019-04-18 01:09:32 -0400

    dao

--- Taller2lab2/src/dao/VendedorDao.java
+++ Taller2lab2/src/dao/VendedorDao.java
@@ -5,10 +5,52 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import modelo.Vendedor;
+
 /**
  *
  * @author cesar
  */
 public class VendedorDao {
+    Conexion con;
+
+    public VendedorDao(Conexion con) {
+        this.con = con;
+    }
     
+    public ArrayList<Vendedor> getVendedores(){
+        ArrayList<Vendedor> vendedores = new ArrayList<>();
+        Connection accesoBD = con.getConexion();
+        try{
+            String sql="SELECT * FROM vendedor ";
+            
+            //System.out.println(sql);
+            Statement st = accesoBD.createStatement();
+            ResultSet resultados = st.executeQuery(sql);
+            
+            while ( resultados.next() ) {
+                int id= Integer.parseInt(resultados.getString("id"));
+                String nombre = resultados.getString("nombre");
+                String apellido = resultados.getString("apellido");
+                String rut = resultados.getString("rut");               
+                
+                vendedores.add(new Vendedor(id, nombre, apellido, rut));
+            }
+            accesoBD.close();
+            return vendedores;
+            
+        }catch(Exception e){
+            System.out.println();
+            System.out.println("Error al obtener");
+            e.printStackTrace();
+            return null;
+        }
+        
+        
+        
+    } 
 }
--- Taller2lab2/src/dao/VentaDao.java
+++ Taller2lab2/src/dao/VentaDao.java
@@ -1,8 +1,61 @@
 
 package dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import modelo.Vendedor;
+
+import modelo.Venta;
+
 
 public class VentaDao {
     /*INSERT INTO `venta` (`id_venta`, `sucursal`, `monto`, `fecha`, `id_vendedor`)
     VALUES (NULL, 'portal', '20000', '2019-04-17', '2');*/
+    
+    Conexion con;
+
+    public VentaDao(Conexion con) {
+        this.con = con;
+    }
+    
+     public ArrayList<Venta> getVenta(){
+        ArrayList<Venta> ventas = new ArrayList<>();
+        Connection accesoBD = con.getConexion();
+        try{
+            String sql="SELECT * FROM venta ";
+            
+            //System.out.println(sql);
+            Statement st = accesoBD.createStatement();
+            ResultSet resultados = st.executeQuery(sql);
+            
+            while ( resultados.next() ) {
+                
+                
+//                ***Vendedor vendedor = resultados.getObject(Vendedor);
+//                
+                
+                Date fecha = resultados.getDate(sql);
+                
+                int id= Integer.parseInt(resultados.getString("id"));
+                
+                
+                
+//                ventas.add(new Venta(apellido, sql, fecha, id));
+            }
+            accesoBD.close();
+            return ventas;
+            
+        }catch(Exception e){
+            System.out.println();
+            System.out.println("Error al obtener");
+            e.printStackTrace();
+            return null;
+        }
+        
+        
+        
+    } 
 }
